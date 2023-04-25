6b6dfd5e4a9b1a8f0b4a4cee67ce3c51cb023153
oozoemenam <oozoemenam@gmail.com>
2023-04-25 20:08:37 +0200

    binary search

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        reverseLinkedList();
+        System.out.print(binarySearch(new int[]{1, 2, 3, 4, 5}, 1, 5, 5));
     }
 
     public static String reverse(String in) {
@@ -110,5 +110,25 @@ public class Main {
         LinkedList<Integer> newList = new LinkedList<>();
         list.descendingIterator().forEachRemaining(newList::add);
         System.out.println(newList);
+    }
+
+    public static int binarySearch(int[] arr, int low, int high, int key) {
+        int mid = (low + high) / 2;
+        while (low <= high) {
+            if (arr[mid] < key) {
+                low = mid + 1;
+            }
+            else if (arr[mid] == key) {
+                return mid;
+            }
+            else {
+                high = mid - 1;
+            }
+            mid = (low + high) / 2;
+        }
+        if (low > high) {
+            return -1;
+        }
+        return -1;
     }
 }
